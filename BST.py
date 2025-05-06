acfa31c4be6d9f79b77e128be9893a00b7d9848f
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-05-06 19:44:51 +0530

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insert(root.left, key)
+        elif key > root.key:
+            root.right = self._insert(root.right, key)
+        return root
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, root, key):
+        if root is None or root.key == key:
+            return root
+        if key < root.key:
+            return self._search(root.left, key)
+        return self._search(root.right, key)
+
+    def inorder(self):
+        return self._inorder(self.root)
+
+    def _inorder(self, root):
+        if root is None:
+            return []
+        return self._inorder(root.left) + [root.key] + self._inorder(root.right)
+
+bst = BST()
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+print("Inorder traversal:", bst.inorder())
+found = bst.search(60)
+print("Search 60:", "Found" if found else "Not found")
